f32afcbb628ae0258b9236dc8e8a4d4aa4114e19
Josh Wilmes <jwilmes@zenoss.com>
2019-02-28 11:59:31 -0500

    Fix device status to match current ZPL behavior, which works on devices that do not have manageIPs set.

--- ZenPacks/zenoss/OpenStack/Endpoint.py
+++ ZenPacks/zenoss/OpenStack/Endpoint.py
@@ -1,7 +1,7 @@
 ###########################################################################
 #
 # This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2011, Zenoss Inc.
+# Copyright (C) 2011-2019, Zenoss Inc.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 2 or (at your
@@ -13,6 +13,12 @@
 
 from Products.ZenModel.Device import Device
 from Products.ZenRelations.RelSchema import ToManyCont, ToOne
+from Products import Zuul
+from zenoss.protocols.protobufs.zep_pb2 import (
+    STATUS_NEW, STATUS_ACKNOWLEDGED,
+    SEVERITY_CRITICAL,
+)
+
 
 class Endpoint(Device):
     meta_type = portal_type = 'OpenStackEndpoint'
@@ -37,3 +43,24 @@ class Endpoint(Device):
 
     def getIconPath(self):
         return '/++resource++openstack/img/openstack.png'
+
+    def getStatus(self, statusclass="/Status/*", **kwargs):
+        # This is identical the behavior provided by ZPL- once this zenpack
+        # is converted to ZPL, this method can be removed.
+        if not self.monitorDevice():
+            return None
+
+        zep = Zuul.getFacade("zep", self.dmd)
+        try:
+            event_filter = zep.createEventFilter(
+                tags=[self.getUUID()],
+                element_sub_identifier=[""],
+                severity=[SEVERITY_CRITICAL],
+                status=[STATUS_NEW, STATUS_ACKNOWLEDGED],
+                event_class=filter(None, [statusclass]))
+
+            result = zep.getEventSummaries(0, filter=event_filter, limit=0)
+        except Exception:
+            return None
+
+        return int(result['total'])
